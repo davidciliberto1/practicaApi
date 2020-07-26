b14783340d88273813ced6c7597488857ae1b8bb
davidciliberto1 <davidciliberto@gmail.com>
2020-07-26 11:50:49 -0300

    practicando

---
+++ src/components/quotes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+const quotes = [
+    'camaron que se duerme se lo lleva la corriente',
+   'arbol que nace torcido jamas su rama endereza',
+   'a muchacho barrigon ni que lo fajen chiquito'
+]
+function Quotes() {
+
+return (
+    <div>
+    <h1>{quotes[1]}</h1>
+    <button 
+    
+    
+    >Press</button>
+    </div>
+)
+}
+export default Quotes;
