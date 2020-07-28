a8349c76b3cb07cb4fc58c9696966a6936d7253c
davidciliberto1 <davidciliberto@gmail.com>
2020-07-28 11:03:45 -0300

    array de cartas

--- src/components/cartas.jsx
+++ src/components/cartas.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'
-const cartas = [
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 'j', 'q', 'k', 'a'
+const carta = [
+     '2t', '3t', '4t', '5t', '6t', '7t', '8t', '9t', '10t', 'jt', 'qt', 'kt', 'at',
+     '2p', '3p', '4p', '5p', '6p', '7p', '8p', '9p', '10p', 'jp', 'qp', 'kp', 'ap',
+     '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'jc', 'qc', 'kc', 'ac',
+     '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', 'jd', 'qd', 'kd', 'ad',
 ]
 
 function Cartas() {
-    const [carta, setCarta] = useState(0)
+    const [cartas, setCarta] = useState(0)
     return(
         <div>
-            <h1>{carta}</h1>
+            <h2>{carta[cartas]}</h2>
+            <h1>Suma: {cartas}</h1>
+            <button
+            onClick={(event) => setCarta(cartas + Math.floor(Math.random()*10))}
+            >random</button>
         </div>
     )
 }
