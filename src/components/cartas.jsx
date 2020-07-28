0b6bcf394f7b3e04906fc17c3df1e2bf45635754
davidciliberto1 <davidciliberto@gmail.com>
2020-07-28 15:54:47 -0300

    ramdon carta2

--- src/components/cartas.jsx
+++ src/components/cartas.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-const carta = [
+const cartas = [
      '2t', '3t', '4t', '5t', '6t', '7t', '8t', '9t', '10t', 'jt', 'qt', 'kt', 'at',
      '2p', '3p', '4p', '5p', '6p', '7p', '8p', '9p', '10p', 'jp', 'qp', 'kp', 'ap',
      '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'jc', 'qc', 'kc', 'ac',
@@ -9,28 +9,19 @@ const carta = [
 
 
 function Cartas() {
-    const [cartas, setCarta] = useState('');
-    const [suma, setSuma] =useState('');
-    console.log(suma);
-    function handleChange(e) {
-        return (
-        cartas + suma
-        )
-        
-       
-    }
+    const [carta, setCarta] = useState('');
+    console.log(carta);
+
 
     return(
         <div>
-            <h2>{cartas}</h2>
+            <h2>{carta}</h2>
            
             <button
-            onClick={(event) => setCarta(carta[Math.floor(Math.random()*52)])}
+            onClick={(event) => cartas.pop(setCarta(cartas[Math.floor(Math.random()*52)]))}
             
             >random</button>
-             <h1
-            onSubmit={(event) => setSuma(event.target.value + cartas)}
-            >Suma: {suma}</h1>
+           
             
         </div>
     )
